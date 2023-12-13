a4263d6822cfc9c9e59f59c0636d55dc8c845cdc
Mikhail <misha-sokolov2004@yandex.ru>
2023-12-13 07:07:28 +0300

    add command

---
+++ Command.cs
@@ -0,0 +1,29 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstPlugin
+{
+    internal class Command : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public string Name => "test";
+
+        public string Help => "";
+
+        public string Syntax => "";
+
+        public List<string> Aliases => new List<string> { "T" };
+
+        public List<string> Permissions => new List<string> { "Test" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            
+        }
+    }
+}
