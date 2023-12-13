e5f2777558b592b05f53a442fafd659e118659a0
Mikhail <misha-sokolov2004@yandex.ru>
2023-12-13 06:45:24 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstPlugin
+{
+    public class Class1
+    {
+    }
+}
